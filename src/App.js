21f80d850cd177992e7800d06c5dae01d44c6c2c
Lherajt <64788512+Lherajt@users.noreply.github.com>
2022-08-10 17:44:29 -0400

    Company component done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { useState } from "react";
 // import { ReactDOM } from "react";
 import "./App.css";
 import StartScreen from "./startScreen";
+import Company from "./Company";
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [screenSubmitted, setScreenSubmitted] = useState("");
 
   console.log(screenSubmitted);
   return (
-    <>
-      {(screenSubmitted === "" || screenSubmitted === "back") && (
+    <Routes>
+        <Route path='/' element={(screenSubmitted === "" || screenSubmitted === "back") && (
         <StartScreen userSubmitted={setScreenSubmitted} />
-      )}
-    </>
+      )} />
+        <Route path='/company' element={<Company />} />
+      </Routes>
+
   );
 }
 
---
+++ src/InputField.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function InputField({ label, type, name, active = '' }) {
+    return (
+        <div className='position-relative p-0'>
+            <label htmlFor={name}>{label}</label>
+            <input
+                className={`input-field`}
+                type={type}
+                name={name}
+                placeholder={`Enter ${label.toLowerCase()}...`}
+                autoFocus={active === 'focused'}
+            />
+            <div className='focus-border position-absolute'></div>
+        </div>
+    )
+}
